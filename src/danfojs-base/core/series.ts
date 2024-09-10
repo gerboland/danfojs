2d911f6b7fbbf2e3150dc0c5b24727b991b71440
Gerry Boland <gerboland@gmail.com>
2024-09-10 12:42:22 +0100

    Type fixes

--- src/danfojs-base/core/series.ts
+++ src/danfojs-base/core/series.ts
@@ -713,7 +713,7 @@ export default class Series extends NDframe implements SeriesInterface {
     */
     std(): number {
         const values = this.$checkAndCleanValues(this.values as ArrayType1D, "max")
-        return std(values);
+        return std(values) as number;
     }
 
     /**
@@ -727,7 +727,7 @@ export default class Series extends NDframe implements SeriesInterface {
     */
     var(): number {
         const values = this.$checkAndCleanValues(this.values as ArrayType1D, "max")
-        return variance(values);
+        return variance(values) as number;
     }
 
     /**
